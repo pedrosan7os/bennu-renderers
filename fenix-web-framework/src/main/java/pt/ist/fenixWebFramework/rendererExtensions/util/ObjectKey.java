4493bf86f9a8611e794ba27b2c0f0d3a447e7e64
Pedro Santos <pedro.san7os@gmail.com>
2013-05-07 16:26:56 +0100

    Bug Fix: ObjectKey had a fragile hashcode and equals

--- fenix-web-framework/src/main/java/pt/ist/fenixWebFramework/rendererExtensions/util/ObjectKey.java
+++ fenix-web-framework/src/main/java/pt/ist/fenixWebFramework/rendererExtensions/util/ObjectKey.java
@@ -1,5 +1,7 @@
 package pt.ist.fenixWebFramework.rendererExtensions.util;
 
+import java.util.Objects;
+
 public class ObjectKey {
 
     private final String externalId;
@@ -22,25 +24,17 @@ public class ObjectKey {
 
     @Override
     public boolean equals(Object other) {
-        if (!(other instanceof ObjectKey)) {
-            return false;
+        if (other instanceof ObjectKey) {
+            if (!Objects.equals(type, ((ObjectKey) other).type)) {
+                return false;
+            }
+            return Objects.equals(externalId, ((ObjectKey) other).externalId);
         }
-
-        ObjectKey otherKey = (ObjectKey) other;
-
-        if (this.type == null && otherKey.type != null) {
-            return false;
-        }
-
-        if (this.type != null && !this.type.equals(otherKey.type)) {
-            return false;
-        }
-
-        return this.externalId == null ? otherKey.externalId == null : this.externalId.equals(otherKey.externalId);
+        return false;
     }
 
     @Override
     public int hashCode() {
-        return this.externalId.hashCode() + (this.type == null ? 0 : this.type.hashCode());
+        return externalId != null ? externalId.hashCode() : type.hashCode();
     }
 }
